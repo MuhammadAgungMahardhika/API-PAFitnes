b4d9098ff4eb55a4df96856411ebbe4f5b01eecf
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2021-12-23 22:36:56 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ $router->get('/', function () use ($router) {
 
 $router->get('/data', function () use ($router) {
     
-  $results = (object) $results;
+ 
     $results = app('db')->select("SELECT * FROM fitnes");
     return response()->json($results);
 });
