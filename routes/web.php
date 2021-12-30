5f229cb39890b2d7663fdaecd2840043129f09ec
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2021-12-30 03:56:25 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,15 @@ $router->post('past', function (Illuminate\Http\Request $request) {
         $id = $request->input('id');
         $nama_fitnes = $request->input('nama_fitnes');
         $alamat_fitnes = $request->input('nama_fitnes');
-       $fasilitas = $request->input('fasilitas');
-      
-        $result = DB::insert("INSERT INTO fitnes (id,nama_fitnes,alamat_fitnes,fasilitas) VALUES ($id,'$nama_fitnes','$alamat_fitnes','$fasilitas')");
+        $fasilitas = $request->input('fasilitas');
+        $harga_perbulan = $request->input('harga_perbulan');
+        $no_fitnes = $request->input('no_fitnes');
+        $jam_buka = $request->input('jam_buka');
+        $gambar_fitnes = $request->input('gambar_fitnes');
+
+        $result = DB::insert("INSERT INTO fitnes (id,nama_fitnes,alamat_fitnes,fasilitas,
+        harga_perbulan,no_fitnes,jam_buka,gambar_fitnes)
+        VALUES ($id,'$nama_fitnes','$alamat_fitnes','$fasilitas','$harga_perbulan','$no_fitnes','$jam_buka','$gambar_fitnes')");
         return ($result)? "wow":"Noo";
     });
 
