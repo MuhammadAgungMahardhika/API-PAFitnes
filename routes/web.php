5409dbc9036022390d062618193a6a3193775750
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2021-12-30 03:11:18 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ $router->get('/data', function () use ($router) {
     return response()->json($results);
 });
 
-$router->post('/post/{id:}/{namaFitnes:}', function ($id,$namaFitnes){
+$router->post('/post/{id}/{namaFitnes}', function ($id,$namaFitnes){
 
     $results_post = app('db')->insert("INSERT INTO fitnes (id, nama_fitnes)
 VALUES ($id,'$namaFitnes')");
