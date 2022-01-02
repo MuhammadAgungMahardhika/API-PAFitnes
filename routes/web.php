30e4205e6c6e954c6ca3b8aead80e5b48dee2041
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2022-01-02 06:03:28 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -38,43 +38,10 @@ $router->post('/postFitnes', function (Illuminate\Http\Request $request) {
         $result = DB::insert("INSERT INTO fitnes (id,nama_fitnes,alamat_fitnes,fasilitas,
         harga_perbulan,no_fitnes,jam_buka,gambar_fitnes)
         VALUES ($id,'$nama_fitnes','$alamat_fitnes','$fasilitas','$harga_perbulan','$no_fitnes','$jam_buka','$gambar_fitnes')");
-     if($result){
-         notif();
-     
-     }   
-    
+  
     return ($result)? "wow":"Noo";
     });
-public function notif(){
- $curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://pafitnes.herokuapp.com/data',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_POSTFIELDS =>'{
-    "to":"fBElW_ghTvyjShkwd7F_ao:APA91bH1qgpIpus2O-6WNfL10EXQesd3zhlddw6x0jc58IhDqeJULwPMG9BUWE-ChtI_MeByyxTccMyt-7KbeglQvzHhak4tRk9E6p2h_bzIylgSdk7otzyziblhdsEVz8jmkNMtYXEL"
-    "notification":{
-    "title": "Succes booking class",
-    "body": "You have succesfull booking the class"
-    }
-   
-}',
-  CURLOPT_HTTPHEADER => array(
-    'Authorization: key=AAAAvCm4eeo:APA91bEYAW38FdnuzDrDLmr1uisn2cupenc4JPPErX-Nc15Y3eqtFO5BXKV5vtWVGTWncYfr4xtpzyschaRlZvrvaIRAMzSy-flmfof_QjEJ7yL6D1Wj0NdE6C4tn9jIEyVDj4un95vE',
-    'Content-Type: application/json'
-  ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);   
-}
 
 //menambah booking
 $router->post('/book', function (Illuminate\Http\Request $request) {
