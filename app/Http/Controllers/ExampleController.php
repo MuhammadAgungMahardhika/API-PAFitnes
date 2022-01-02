38a9618e89b5d2edd740a3dd9c00193451c87f0d
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2022-01-02 06:41:25 -0800

    Update ExampleController.php

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -24,6 +24,23 @@ class ExampleController extends Controller
     return response()->json($results);
     }
     
+    public function addFitnes(Illuminate\Http\Request $request){
+         $id = $request->input('id');
+        $nama_fitnes = $request->input('nama_fitnes');
+        $alamat_fitnes = $request->input('nama_fitnes');
+        $fasilitas = $request->input('fasilitas');
+        $harga_perbulan = $request->input('harga_perbulan');
+        $no_fitnes = $request->input('no_fitnes');
+        $jam_buka = $request->input('jam_buka');
+        $gambar_fitnes = $request->input('gambar_fitnes');
+    
+        $result = DB::insert("INSERT INTO fitnes (id,nama_fitnes,alamat_fitnes,fasilitas,
+        harga_perbulan,no_fitnes,jam_buka,gambar_fitnes)
+        VALUES ($id,'$nama_fitnes','$alamat_fitnes','$fasilitas','$harga_perbulan','$no_fitnes','$jam_buka','$gambar_fitnes')");
+  
+    return ($result)? "wow":"Noo";
+    }
+    
     public function notif(){
     $curl = curl_init();
 
