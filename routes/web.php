6847d840d66a119c8147825013d5eee2e535a1dd
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2022-01-03 16:31:55 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -50,11 +50,7 @@ $router->post('/book', function (Illuminate\Http\Request $request) {
     
         $result = DB::insert("INSERT INTO detail_booking (id_fitnes,id_user)
         VALUES ($id_fitnes,$id_user)");
-    
-    
-        if($result){
-        $this->'ExampleController@notif';
-        }      
+     
         return ($result)? "wow":"Noo";
     });
 
