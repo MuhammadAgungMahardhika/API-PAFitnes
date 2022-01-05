ce144c9878a680f2ba1ce68a9ab213ae2f7e1894
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2022-01-04 16:49:12 -0800

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+   protected $table = 'posts';
+    protected $fillable = array('title','content','status','user_id');
+    
+    public $timestamps = true;
+}
