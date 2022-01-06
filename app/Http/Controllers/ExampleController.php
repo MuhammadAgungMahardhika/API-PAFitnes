a10115c6fac6447506aee9b8244282de95d74b9a
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2022-01-06 12:01:56 +0700

    Update ExampleController.php

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -27,13 +27,14 @@ class ExampleController extends Controller
     }
     
     public function bookFitnes(Request $request){
-       
+      
         $id_fitnes = $request->input('id_fitnes');
         $id_user = $request->input('id_user');
-        $result = DB::insert("INSERT INTO detail_booking (id_fitnes,id_user)
+        
+        $result = app('db')->insert("INSERT INTO detail_booking (id_fitnes,id_user)
         VALUES ($id_fitnes,$id_user)");
         
-        return ($result)? "wow":"Noo";
+        return response()->json($result);
     }
 
         
